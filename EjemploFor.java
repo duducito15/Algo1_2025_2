2b8d1f76c7b6329019105f426c22c6603da86d29
duducito15 <echavezv@utea.edu.pe>
2025-11-17 09:20:03 -0500

    bucles

---
+++ EjemploFor.java
@@ -0,0 +1,9 @@
+public class EjemploFor {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) { // i = i + 2;
+            System.out.println(i);
+        }
+
+    }
+}
