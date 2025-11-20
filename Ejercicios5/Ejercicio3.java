097c3a380ef80cbf44f7f42d4bfee63ae25e182c
duducito15 <echavezv@utea.edu.pe>
2025-11-20 11:00:04 -0500

    Ejercicio 4

---
+++ Ejercicios5/Ejercicio3.java
@@ -0,0 +1,15 @@
+package Ejercicios5;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        // do while
+        int i = 0;
+
+        do {
+            if (i%5==0) {
+                System.out.println(i);
+            }
+            i++;
+        } while (i < 101);
+    }
+}
---
+++ Ejercicios5/Ejercicio4.java
@@ -0,0 +1,10 @@
+package Ejercicios5;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        // Muestra numeros del 320 al 160 de 20 en 20 hacia atras for
+        for (int i = 320; i > 159; i = i - 20) {
+            System.out.println(i);
+        }
+    }
+}
