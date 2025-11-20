ef36198a421470ecd64f0ddf10479c130347816a
Duducito15 <echavezv@utea.edu.pe>
2025-11-20 10:43:05 -0500

    Ejercicios 5

---
+++ Ejercicios5/Ejercicio1.java
@@ -0,0 +1,13 @@
+package Ejercicios5;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // multiplos de 5 del 0 al 100 usando el for
+        for (int i = 0; i < 101; i = i + 5) {
+            // if (i % 5 == 0) {
+            // System.out.println(i);
+            // }
+            System.out.println(i);
+        }
+    }
+}
---
+++ Ejercicios5/Ejercicio2.java
@@ -0,0 +1,16 @@
+package Ejercicios5;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        // Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+        int i = 0;
+
+        while (i < 101) {
+            //if (i % 5 == 0) {
+            //    System.out.println(i);
+            //}
+            System.out.println(i);
+            i= i + 5;
+        }
+    }
+}
