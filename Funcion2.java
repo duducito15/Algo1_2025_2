3ab2fded2528c729d50989c9d362d8b891080877
duducito15 <echavezv@utea.edu.pe>
2025-12-22 09:25:09 -0500

    Funcion 2 esPrimo

--- Funcion2.java
+++ Funcion2.java
@@ -2,17 +2,20 @@ public class Funcion2 {
     public static void main(String[] args) {
         System.out.print("Introduce un número entero positivo: ");
         int n = Integer.parseInt(System.console().readLine());
-        boolean esPrimo = true;
-        
-        for (int i = 2; i < n; i++) {
-            if ((n % i) == 0) {
-                esPrimo = false;
-            }
-        }
-        if (esPrimo) {
+      
+        if (esPrimo(n)) {
             System.out.println("El " + n + " es primo.");
         } else {
             System.out.println("El " + n + " no es primo.");
         }
+    }
+
+    public static boolean esPrimo(int num) {
+        for (int i = 2; i < num; i++) {
+            if ((num % i) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
